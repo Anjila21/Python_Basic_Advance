22685f8140b12d7f5685bf84da77c8bd934a5eef
Dell <anjila.tripathi@gmail.com>
2023-09-12 08:46:48 +0545

    numpy

---
+++ Python_Basic_Advance/Hello World.py
@@ -0,0 +1,13 @@
+print("This 1st python Project")
+print(5**5)
+print(15//4)
+
+x= 10
+y = 2
+print(bin(x))
+
+anjila = """
+   My name is Anjila.
+   Anjila is my name.
+    """
+print(anjila)
---
+++ Python_Basic_Advance/numpyex.py
@@ -0,0 +1,20 @@
+#  Numpy stands for numerical python
+#  There are various types of data that needs to be converted into machine undestandable
+# for this numpy converts the given data into array and matrices.
+
+import numpy as np
+#For 1 Dimensional array
+_1darray = np.array([1,2,3,4])
+print(_1darray)
+print(type(_1darray))
+print(_1darray.ndim)
+
+#For 2-D array
+_2darray = np.array([[1,2,3,4,5],[7,8,9,10,11]])
+print(_2darray)
+print(type(_2darray))
+print(_2darray.ndim)
+
+
+
+
