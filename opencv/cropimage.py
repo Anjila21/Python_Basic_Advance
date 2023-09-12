951ef05859f72f087bab0917725986a3f16e45bd
Anjila Tripathi <121032429+Anjila21@users.noreply.github.com>
2023-09-12 10:46:18 +0545

    Delete opencv directory

--- opencv/cropimage.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-image = cv2.imread('panda.jpg')
-print(image.shape)
-resize = cv2.resize(image,(400,400))
-imgcrop = image[200:400, 500:1000]
-cv2.imshow('output',resize)
-cv2.imshow('cropped image',imgcrop)
-cv2.waitKey(0)
--- opencv/merge image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-img = cv2.imread('ss1.png')
-
-imgVer = np.vstack((img,img))
-
-cv2.imshow('output',imgVer)
-cv2.waitKey(0)
