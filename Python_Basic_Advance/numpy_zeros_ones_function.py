2e4998c264e77130626f6282ed395903a8458147
Dell <anjila.tripathi@gmail.com>
2023-09-12 09:11:55 +0545

    zeros//ones//empty function

---
+++ Python_Basic_Advance/numpy_zeros_ones_function.py
@@ -0,0 +1,12 @@
+import numpy as np
+#zero function : To make every element of row and column zero
+zero = np.zeros((3,4),dtype= int)
+print(zero)
+
+#one function: To make every element of row and column one
+one= np.ones((3,4))
+print(one)
+
+#empty matrix : It gives garbage value as output
+empty= np.empty((3,3))
+print(empty)
