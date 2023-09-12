68d15dbea507c08fe2d1f6b676a4d0d803becc5c
Dell <anjila.tripathi@gmail.com>
2023-09-12 09:44:58 +0545

    numpy functions

---
+++ Python_Basic_Advance/numpy_functions.py
@@ -0,0 +1,27 @@
+#Numpy functions
+
+import numpy as np
+
+#arange()
+include=np.arange(1,50,4)
+print(include)
+
+#linspace()
+space = np.linspace(1,2,5)
+print(space)
+
+#reshape(): converts array into multidimensional array
+array1= np.array([1,2,3,4,5,6])
+reshape = array1.reshape(2,3)
+print(reshape)
+
+array2 = np.array([[1,2,2,2,3,1],[4,5,6,7,8,4]])
+reshape2 = array2.reshape(4,3)
+print(reshape2)
+
+#ravel() and flatten(): converts multi-dimension array into 1D array
+print(array2.ravel())
+print(array2.flatten())
+
+#transpose() : exchanges the rows and column of the inital matraix
+print(reshape2.transpose())
