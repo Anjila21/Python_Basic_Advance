ae3d6f8697839f2a60df16e0427ee28971ffd28e
Dell <anjila.tripathi@gmail.com>
2023-09-20 16:49:23 +0545

    class and obj

---
+++ oop/class_and_obj.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self,name,age,address):
+        self.name= name
+        self.age= age
+        self.address= address
+    def StudentInfo(self):
+       # return("Name:, {self.name}"Age:",{self.age}"Address:",{self.address})
+        return f"Name: {self.name}, Age: {self.age}, Address: {self.address}"
+
+student = Student("Anjila",22,"Kalanki")
+print(student.StudentInfo())
