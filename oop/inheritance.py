5ac2db17a274aa8a743acfc40859ab50d9d4d103
Dell <anjila.tripathi@gmail.com>
2023-09-20 11:45:32 +0545

    Inheritance

---
+++ oop/inheritance.py
@@ -0,0 +1,17 @@
+#single inheritance
+#Multi-level inheritance
+#multiple inheritance
+
+class A:
+    def a(self):
+        print("This is Class A")
+class B(A):  #Class 'A' is inheritant to class 'B'
+    def b(self):
+        print("This is Class B")
+class C(B):   #Class 'B' is inheritant to class 'C'
+    def c(self):
+        print("This is Class C")
+classes = C()
+classes.a()
+classes.b()
+classes.c()
